bfd5a1b4cfccc67864e359edd466131f97adbb40
Michael Fahey <michael@gravitie.cloud>
2022-04-25 14:29:18 +0100

    Added addToCart feature

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,11 +1,27 @@
-import './cart-dropdown.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button';
+import CartItem from '../cart-item/cart-item';
+
+import './cart-dropdown.scss';
+
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items" />
+            <div className="cart-items">
+                {cartItems.length ? (
+                    cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    ))
+                ) : (
+                    <span className="empty-message">Your cart is empty</span>
+                )}
+            </div>
             <Button>Go to Checkout</Button>
         </div>
     )
